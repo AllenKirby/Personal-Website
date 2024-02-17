7141f6bfc29c88b06ed48bb072e1549fc44fe064
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2024-02-17 13:58:51 +0800

    asjabjsa

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -17,39 +17,39 @@ const Skills = () => {
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-html bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className=" md:xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">HTML</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-css bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">CSS</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-js bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">Javascript</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-react bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">React</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-tailwind bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">Tailwind</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-git bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">Git</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-python bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">Python</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-firebase bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">Firebase</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-sql bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">SQL</p>
                 </div>
-                <div className="w-auto h-auto text-center m-5">
+                <div className="w-auto h-auto text-center m-3">
                     <div className="md:w-20 md:h-20 w-14 h-14 m-3 bg-csharp bg-cover cursor-pointer transition-all duration-300"></div>
                     <p className="md:text-xl text-lg font-poppins p-3 font-medium text-blue-950 transition-all duration-300">C#</p>
                 </div>
