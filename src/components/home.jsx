ae7bc8ab0e243e44045a6bdbff2d2a7ec0de009a
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2024-02-18 12:42:47 +0800

    make changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,18 +2,20 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 
-const Home = () =>{
+import ME from '../assets/images/me.JPG'
+
+const Home = () => {
     useEffect(() => {
 
         AOS.init()
 
     }, []);
-    return(
+    return (
         <section id="home" className="h-screen flex items-center justify-center">
             <article className="w-auto h-auto flex items-center justify-start p-10" data-aos="fade-up">
                 <div>
                     <figure className="w-full h-auto flex justify-center items-center p-10">
-                        <div className="md:w-52 md:h-52 w-40 h-40 bg-gray-950 bg-myself bg-cover rounded-full transition-all duration-300"></div>  
+                        <div className="md:w-52 md:h-52 w-40 h-40 bg-gray-950 bg-cover rounded-full transition-all duration-300" style={{ backgroundImage: `url(${ME})` }}></div>
                     </figure>
                     <div className="text-center">
                         <p className="md:text-4xl text-3xl font-normal text-blue-950 font-poppins transition-all duration-300">I am Allen Kirby V. Santileces</p>
