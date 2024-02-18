cce2e0b235484aa9e440dc3afb785e2bad956ad0
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2024-02-18 21:35:34 +0800

    ''

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -16,10 +16,10 @@ import git from '../assets/images/git.png'
 
 const Skills = () => {
     useEffect(() => {
-        AOS.init()
+        AOS.init()  
     }, [])
     return(
-        <section id="skills" className="h-auto flex flex-col items-center justify-center py-52">
+        <section id="skills" className="h-auto flex flex-col items-center justify-center py-44">
             <article className="w-full h-auto text-center" data-aos="fade-down">
                 <p className="md:text-5xl text-4xl font-poppins font-medium text-blue-950 transition-all duration-300 my-5" >Skills</p>
                 <p className="md:text-2xl text-xl font-poppins mx-8 font-light text-blue-950 transition-all duration-300 my-5">Continually enhancing and expanding expertise in these languages to achieve mastery.</p>
